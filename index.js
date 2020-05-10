4386b015e3431ecade11f2413aabe36deed1130a
Abhishek Jangid <abhishek@indwealth.in>
2020-05-10 23:30:00 +0530

    ADD: added daatabase connection and env file

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
-const express = require('express')
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+require('dotenv').config();
 
-const app = express()
+const port = 5000
 
-const port = 3003
+// middleware
+app.use('/posts', () => {console.log('THis is middleware running')})
 
-app.get('/', (req, res) => res.send("Hello world"))
+// database
+const conn = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER_NAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+})
 
-app.listen(port, () => console.log('>>>>>>>'))
+conn.connect((err) => {
+    if (err) throw err;
+    console.log('DB Connection success')
+});
+
+
+conn.query("show databases", (err, result, fields) => {
+    console.log(err);
+    console.log(result);
+    // console.log(`fields ${fields}`);
+})
+
+app.get('/', (req, res) => {
+    res.send("<h1>Hello worasdld</h1>");
+});
+
+
+app.get('/posts', (req, res) => {
+    res.send("Hello postssssss");
+});
+
+
+app.listen(port);
